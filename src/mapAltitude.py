c374dabb845364f82eb90a1e2df6822101399455
Eduardo Santana <ezambomsantana@gmail.com>
2020-02-04 20:50:09 -0300

    altitude

--- src/mapAltitude.py
+++ src/mapAltitude.py
@@ -1,8 +1,7 @@
 import xmltodict
 import utm
-
-def calculate_distance(lat, lon):  
-    return utm.to_latlon(lat, lon, 23, 'K')
+import pyproj
+import openrouteservice
 
 with open('../data/map.xml') as fd:
     doc = xmltodict.parse(fd.read())
@@ -11,6 +10,16 @@ with open('../data/map.xml') as fd:
         print(element['@id'])
         x = element['@x'].split(".")[0]
         y = element['@y'].split(".")[0]
-        print(x)
 
-        print(calculate_distance(x, y))
+        x1, y1 = pyproj.transform('EPSG:32719', 'wgs84', x, y)
+
+        client = openrouteservice.Client(key='5b3ce3597851110001cf6248221d6f34d0204f47a619667c177f6459') # Specify your personal API key
+
+        print(x1, y1)
+
+        routes = client.elevation_point('point', (y1, x1))
+        print(routes)
+        geometry = routes['geometry']['coordinates'][2]
+
+        print(geometry)
+        break
