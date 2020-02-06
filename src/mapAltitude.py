5d64ec41472580cd8d9b5f4f373f6fcf20f93a40
Eduardo Santana <ezambomsantana@gmail.com>
2020-02-06 10:29:37 -0300

    altitude

--- src/mapAltitude.py
+++ src/mapAltitude.py
@@ -2,13 +2,22 @@ import xmltodict
 import utm
 import pyproj
 import openrouteservice
+import csv
+import pandas as pd
+
+
+teste = pd.read_csv('../data/alts.csv', index_col='id')
+print(teste)
 
 csv_file = "../data/alts.csv"
 mydict = []
 with open(csv_file, mode='r') as infile:
-    reader = csv.reader(infile, delimiter=";")
+    reader = csv.reader(infile, delimiter=",")
     mydict = {rows[0]:rows[1] for rows in reader}
     
+ids = []
+alts = []
+
 with open('../data/map.xml') as fd:
     doc = xmltodict.parse(fd.read())
     
@@ -22,9 +31,10 @@ with open('../data/map.xml') as fd:
             x = element['@x'].split(".")[0]
             y = element['@y'].split(".")[0]
 
+
             x1, y1 = pyproj.transform('EPSG:32719', 'wgs84', x, y)
 
-            client = openrouteservice.Client(key='5b3ce3597851110001cf6248221d6f34d0204f47a619667c177f6459') # Specify your personal API key
+            client = openrouteservice.Client(key='5b3ce3597851110001cf624810370a72249c4658ab1b22d88ab2ce4b') # Specify your personal API key
 
             print(x1, y1)
 
@@ -32,13 +42,18 @@ with open('../data/map.xml') as fd:
             print(routes)
             alt = routes['geometry']['coordinates'][2]
 
-            print(geometry)
-
-            mydict[element['@id']] = alt
-
+            ids.append(element['@id'])
+            alts.append(alt)
         except:
             print("An exception occurred")
             break
 
     
-print(mydict)
+frame = pd.DataFrame(list(zip(ids, alts)), columns =['id','alt'])
+frame = frame.set_index('id')
+print(frame)
+teste = teste.append(frame)
+
+print(teste)
+
+teste.to_csv('../data/alts.csv')
