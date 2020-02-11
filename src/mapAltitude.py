ad3c9b0aa933a73946cda867e89f8936bfce4950
Eduardo Santana <ezambomsantana@gmail.com>
2020-02-11 09:07:39 -0300

    alts

--- src/mapAltitude.py
+++ src/mapAltitude.py
@@ -34,7 +34,7 @@ with open('../data/map.xml') as fd:
 
             x1, y1 = pyproj.transform('EPSG:32719', 'wgs84', x, y)
 
-            client = openrouteservice.Client(key='5b3ce3597851110001cf6248221d6f34d0204f47a619667c177f6459') # Specify your personal API key
+            client = openrouteservice.Client(key='5b3ce3597851110001cf62484bb60b7582ea443aa2e89bb73cb2c985') # Specify your personal API key
 
             print(x1, y1)
 
